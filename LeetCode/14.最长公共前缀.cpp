868ad262757d1573d3e946802a33ba9721eb4c41
MiKu-Ming <2412780034@qq.com>
2023-04-16 20:19:33 +0800

    最长公共前缀

---
+++ LeetCode/14.最长公共前缀.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=cpp
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode.cn/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (43.24%)
+ * Likes:    2731
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string res = strs.empty() ? "" : strs[0];
+        for (string s : strs)
+        {
+            while (s.find(res) != 0)
+            {
+                res = res.substr(0, res.length() - 1);
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
