d7fbd64a454ba30f872c0a2c6a4adf3aa7016a67
MiKu-Ming <2412780034@qq.com>
2023-04-21 23:28:58 +0800

    二进制求和

--- cpp-learn/test.cpp
+++ cpp-learn/test.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 int main()
 {
-    cout << "你好" << endl;
+    // cout << "你好" << endl;
+    // system("pause");
+    // return 0;
+    int i;
+    for (i = 10; i >= 0; --i)
+    {
+        cout << i << endl;
+    }
     system("pause");
     return 0;
 }
