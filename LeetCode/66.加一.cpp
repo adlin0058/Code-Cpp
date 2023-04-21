af368fd0a964ad431b83f987d46b69fed0c55935
MiKu-Ming <2412780034@qq.com>
2023-04-21 23:03:17 +0800

    加一

--- LeetCode/66.加一.cpp
+++ LeetCode/66.加一.cpp
@@ -10,7 +10,20 @@ class Solution
 public:
     vector<int> plusOne(vector<int> &digits)
     {
-        int sum = 0;
+
+        // 从最低位开始遍历
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            digits[i]++; // 对当前位加1
+            // 如果当前位等于10，说明需要进位
+            if (digits[i] == 10)
+                digits[i] = 0;
+            else
+                return digits; // 否则直接返回结果
+        }
+        // 如果整个数字都需要进位，需要在数组最前面插入一个1
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
 // @lc code=end
