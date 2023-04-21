4451d20977f917d12e88d9b718cf7b8b8c2b79bd
MiKu-Ming <2412780034@qq.com>
2023-04-21 22:58:37 +0800

    加一

---
+++ LeetCode/66.加一.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int sum = 0;
+    }
+};
+// @lc code=end
