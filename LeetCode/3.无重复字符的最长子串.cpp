9b1d9aeef287a43ede65b61568f704e0ed1500a7
MiKu-Ming <2412780034@qq.com>
2023-04-24 21:25:25 +0800

    无重复字符的最长字符串

---
+++ LeetCode/3.无重复字符的最长子串.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        vector<int> m(128, 0);
+        int ans = 0;
+        int i = 0;
+        for (int j = 0; j < s.size(); j++)
+        {
+            i = max(i, m[s[j]]);
+            m[s[j]] = j + 1;
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
+// @lc code=end
