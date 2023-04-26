507e49c0f54de509c1f61428b6c1d51eb2a82c1d
MiKu-Ming <2412780034@qq.com>
2023-04-26 22:51:49 +0800

    三数之和

---
+++ LeetCode/15.三数之和.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=cpp
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        // 找出a + b + c = 0
+        // a = nums[i], b = nums[j], c = -(a + b)
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // 排序之后如果第一个元素已经大于零，那么不可能凑成三元组
+            if (nums[i] > 0)
+            {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1])
+            { // 三元组元素a去重
+                continue;
+            }
+            unordered_set<int> set;
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (j > i + 2 && nums[j] == nums[j - 1] && nums[j - 1] == nums[j - 2])
+                { // 三元组元素b去重
+                    continue;
+                }
+                int c = 0 - (nums[i] + nums[j]);
+                if (set.find(c) != set.end())
+                {
+                    result.push_back({nums[i], nums[j], c});
+                    set.erase(c); // 三元组元素c去重
+                }
+                else
+                {
+                    set.insert(nums[j]);
+                }
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
